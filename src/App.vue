2fa69e56d163fdbf34f6960721d5b3dd5d070c4a
gi2k15 <gendouikari2015@gmail.com>
2025-10-30 18:05:31 -0300

    Adicionado seletor de salmoura e proteína

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,22 @@ import { computed } from 'vue';
 
 const peso = defineModel('peso');
 const agua = defineModel('agua', { default: 500 });
+const tipo = defineModel('tipo', { default: 7 });
+const salmoura = defineModel('salmoura', { default: 0.1 });
 
 const tempo = computed(() => {
-  return formatarTempo(((peso.value * 7) / 100) * 60); // 7 min para cada 100 gramas
-})
+  return formatarTempo(((peso.value * tipo.value) / 100) * 60);
+});
 const sal = computed(() => {
-  return agua.value * 0.1; // Salmoura 10%
-})
+  return agua.value * salmoura.value;
+});
+const tipoObj = [
+  { text: 'Frango', value: 7 }
+];
+const salmouraObj = [
+  { text: '10%', value: 0.1 },
+  { text: '5%', value: 0.05 }
+]
 
 function formatarTempo(segundos) {
   const horas = Math.floor(segundos / 3600);
@@ -28,9 +37,21 @@ function formatarTempo(segundos) {
 <template>
   <div class="container">
     <h1>Calculadora de salmoura</h1>
-    <p>Esta calculadora ajuda a determinar a quantidade de sal necessária para preparar uma salmoura 10%.</p>
+    <p>Esta calculadora ajuda a determinar a quantidade de sal necessária para preparar uma salmoura.</p>
     <div class="entradas">
       <form>
+        <label for="tipo">Tipo de proteína</label>
+        <select id="tipo" v-model="tipo">
+          <option v-for="t in tipoObj" :value="t.value">
+            {{ t.text }}
+          </option>
+        </select>
+  <label for="salmoura">Concentração da salmoura</label>
+  <select id="salmoura" v-model="salmoura">
+          <option v-for="s in salmouraObj" :value="s.value">
+            {{ s.text }}
+          </option>
+        </select>
         <label for="peso">Peso do alimento (g)</label>
         <input type="number" id="peso" v-model="peso">
         <label for="agua">Quantidade de água (mL)</label>
@@ -41,7 +62,8 @@ function formatarTempo(segundos) {
         <p>Quantidade de sal: {{ sal }} gramas</p>
       </div>
     </div>
-    <p style="text-align: center;"><a href="https://github.com/gi2k15/Calc-salm" target="_blank" noreferrer noopener>GitHub</a></p>
+    <p style="text-align: center;"><a href="https://github.com/gi2k15/Calc-salm" target="_blank" noreferrer
+        noopener>GitHub</a></p>
   </div>
 </template>
 
@@ -56,7 +78,7 @@ h1 {
   text-align: center;
 }
 
-input {
+input, select {
   padding: 5px;
   box-sizing: border-box;
 }
